2ae636225ca1a345a1e18f54acfe3dbec431a67f
Enayatullah Haqqani <zadkiel9067@gmail.com>
2026-04-23 20:20:26 +0430

    Add initial build.gradle.kts configuration

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// فایل: build.gradle.kts
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
