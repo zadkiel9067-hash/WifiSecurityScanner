f2a7317465d27f7a1b74f63f0f96fa564920dbb5
Enayatullah Haqqani <zadkiel9067@gmail.com>
2026-04-23 20:21:58 +0430

    Initialize settings.gradle.kts for project setup

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+// فایل: settings.gradle.kts
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "WifiSecurityScanner"
+include(":app")
